2a1a99291fa4efcf45de996c1462411eb7abe96a
daniel <daniel.nemeth83@yahoo.com>
2021-08-22 18:23:30 +0200

    Adds interface example

---
+++ Functions_methods_interfaces/interfaces.go
@@ -0,0 +1,59 @@
+package main
+import (
+    "fmt"
+)
+
+/*
+ * Interfaces
+ * Set of method signatures
+ *  - name, parameters, return values
+ *  - implementation is NOT defined
+ * Used to express conceptual similarity between types
+ * Example: Shape2D interface - all 2D shapes must have Area() and Perimeter()
+ */
+type Shape2D interface {
+    Area() float64
+    Perimeter() float64
+}
+
+
+/*
+ * Satisfying an Interface
+ * Type satisfies an interface if type defines all methods specified in the interface
+ *  - same method signatures needed
+ *  - additional methods (and data) are OK
+ * Having Interface in go similar to inheritance with overriding
+ */
+type Triangle struct {
+    base float64
+    height float64
+    a float64
+    c float64
+}
+
+
+func (t Triangle) Area() float64 {
+    area := 0.5 * t.base * t.height
+    return area 
+}
+
+
+func (t Triangle) Perimeter() float64 {
+    perimeter := t.base + t.a + t.c
+    return perimeter
+}
+
+
+func main() {
+    myt := Triangle{base:3, height:4, a: 4, c:6}
+    fmt.Println(myt.Area())
+    fmt.Println(myt.Perimeter())
+
+// this technique can be used to check if Triangle satisfies the interface Shape2D    
+    var i interface{} = myt
+    _, flag := i.(Shape2D)
+    fmt.Printf("Triangle satisfies the Shape2D interface: %v\n", flag)
+}
+
+
+
